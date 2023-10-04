546a37da63bee0c2e88d7197b11e7cde45937461
JosephNoz <jn318@sussex.ac.uk>
2023-10-04 17:44:02 +0200

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Joseph Norris on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
